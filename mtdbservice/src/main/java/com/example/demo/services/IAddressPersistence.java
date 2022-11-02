1cfd827c3fff4aa256b9ea59ca16d2bebb84d3f2
ZL538PY <ZL538PY@IN3039537W2.ey.net>
2022-11-02 11:41:18 +0530

    First Commit

---
+++ mtdbservice/src/main/java/com/example/demo/services/IAddressPersistence.java
@@ -0,0 +1,10 @@
+package com.example.demo.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Address;
+
+
+public interface IAddressPersistence extends JpaRepository<Address, String> {
+
+}
